a02d150a5cd23030f1dddaf53ddf15527b123fb2
lucapandolfelli <lucapandolfelli2002@gmai.com>
2023-02-12 08:44:26 -0300

    new changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,15 @@
+'use client'
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function Footer () {
+  const pathname = usePathname()
+  const [showFooter, setShowFooter] = useState('bg-[#2c3440] h-[235px] pt-[1rem]')
+
   return (
-    <footer className='bg-[#2c3440] h-[235px] pt-[1rem]'>
+    <footer className={ pathname == '/login' || pathname == '/register' ? 'hidden' : showFooter}>
       <div className='max-w-5xl mx-auto text-[#99aabb] text-center'>
         <p>&copy; 2023. Designed by <Link href='https://lucapandolfelli.com'>Luca Pandolfelli</Link></p>
       </div>
