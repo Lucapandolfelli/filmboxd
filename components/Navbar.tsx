222758c0630f655ba6bec8762a4f3f5616134b0e
lucapandolfelli <lucapandolfelli2002@gmai.com>
2023-02-12 10:57:54 -0300

    new changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,11 +11,11 @@ const navbarItems = [
 
 export default function Navbar () {
   const pathname = usePathname()
-  const [showNavbar, setShowNavbar] = useState('fixed top-0 left-0 w-full transition-all durantion-500 ease-linear text-[#ffffe9] font-semibold h-[80px] z-50')
+  const [showNavbar, setShowNavbar] = useState('fixed top-0 left-0 w-full px-[1.25rem] md:p-0 transition-all durantion-500 ease-linear text-[#ffffe9] font-semibold h-[80px] z-50')
 
   useEffect(() => {
     const changeNavbarVisibility = () => {
-      window.scrollY >= 100 ? setShowNavbar('bg-[#0e1114] transition-all durantion-500 ease-linear shadow-md fixed top-0 left-0 w-full text-[#ffffe9] font-semibold h-[60px] z-50') : setShowNavbar('fixed top-0 left-0 w-full transition-all durantion-500 ease-linear text-[#ffffe9] font-semibold h-[80px] z-50')
+      window.scrollY >= 100 ? setShowNavbar('bg-[#0e1114] transition-all durantion-500 ease-linear shadow-md fixed top-0 left-0 w-full px-[1.25rem] md:p-0 text-[#ffffe9] font-semibold h-[60px] z-50') : setShowNavbar('fixed top-0 left-0 w-full px-[1.25rem] md:p-0transition-all durantion-500 ease-linear text-[#ffffe9] font-semibold h-[80px] z-50')
     }
     window.addEventListener('scroll', changeNavbarVisibility)
   }, [])
