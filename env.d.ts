f1a70bc0ecb4c618cfa00fbfc4ec7390a27e335d
lucapandolfelli <lucapandolfelli2002@gmai.com>
2023-02-12 14:54:52 -0300

    new changes

--- env.d.ts
+++ env.d.ts
@@ -2,6 +2,8 @@ namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: string;
     NEXT_PUBLIC_HOST: string;
+    NEXT_PUBLIC_APP_NAME: string;
+    NEXT_PUBLIC_SECRET: string;
     NEXTAUTH_SECRET: string;
     NEXTAUTH_URL: string;
     GOOGLE_CLIENT_ID: string;
