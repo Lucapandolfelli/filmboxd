3d2025f3ec356622c8d499e37073b983a5972d26
lucapandolfelli <lucapandolfelli2002@gmai.com>
2023-02-25 17:43:22 -0300

    new changes

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,12 +7,12 @@ interface Props {
 
 export default function UserLayout ({ children }: Props): JSX.Element {
   return (
-    <main className='bg-gradient-to-tr from-[#000] to-[#0e1114] h-screen flex justify-center items-center'>
-      <section className='bg-[#14181c] w-[1000px] h-[700px] flex shadow-2xl rounded-[.75rem] overflow-hidden'>
-        <div className='relative w-[50%] bg-center bg-cover' style={{ backgroundImage: `url('/images/login.jpg')` }}>
+    <main className='bg-gradient-to-tr from-[#000] to-[#0e1114] h-screen flex justify-center items-center px-[1.25rem]'>
+      <section className='bg-[#14181c] max-w-[1000px] w-full h-[700px] flex flex-col md:flex-row shadow-2xl rounded-[.75rem] overflow-hidden'>
+        <div className='h-[300px] md:h-full relative w-full md:w-[50%] bg-center bg-cover' style={{ backgroundImage: `url('/images/login.jpg')` }}>
           <p className='text-sm text-[#ffffe9] font-bold absolute left-[.75rem] bottom-[.5rem]'>Blade Runner 2049</p>
         </div>
-        <div className='w-[50%] flex flex-col justify-center items-center gap-[4rem] text-[#99aabb]'>
+        <div className='w-full p-[2rem] md:w-[50%] flex flex-col justify-center items-center gap-[4rem] text-[#99aabb]'>
           <Link href='/' className='flex items-center gap-[.25rem]'>
             <figure className='w-[60px] h-[20px] relative'>
               <Image src='/images/filmboxd-navbar.png' fill alt='Filmboxd Logo' className='object-contain' />
